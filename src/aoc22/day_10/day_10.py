e880efdb8c27ac0955dfb149f1bb76390537c6af
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-12-09 08:50:16 +0000

    Clean up a bit

--- src/aoc22/day_10/day_10.py
+++ src/aoc22/day_10/day_10.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 from aoc22 import get_logger
 from aoc22 import main
 
@@ -61,28 +63,11 @@ def step(register: Register, lines: list[str], line: str, instruction: str) -> s
     return line
 
 
-def part_1(data: str) -> int:
-    register = Register('X')
-    values = []
-    next_cycle = 20
-    for instruction in data.splitlines():
-        match instruction.split():
-            case ['noop']:
-                register.cycle += 1
-                if register.cycle == next_cycle:
-                    values.append(register.signal_strength)
-                    next_cycle += 40
-            case ['addx', n]:  # pylint: disable=invalid-name
-                register.cycle += 1
-                if register.cycle == next_cycle:
-                    values.append(register.signal_strength)
-                    next_cycle += 40
-                register.value += int(n)
-                register.cycle += 1
-                if register.cycle == next_cycle:
-                    values.append(register.signal_strength)
-                    next_cycle += 40
-    return sum(values)
+def add_value(register: Register, values: list[int], next_cycle: int) -> int:
+    if register.cycle == next_cycle:
+        values.append(register.signal_strength)
+        next_cycle += 40
+    return next_cycle
 
 
 def print_sprite(sprite_position: int) -> None:
@@ -93,68 +78,65 @@ def print_sprite(sprite_position: int) -> None:
     print('Sprite position:', sprite_string)
 
 
+def update(cycle: int, pixel_position: int, sprite_position: int, line: str) -> str:
+    print(
+        f'During cycle {cycle:2}:',
+        f'CRT draws pixel in position {pixel_position}',
+    )
+    if abs(pixel_position - sprite_position) <= 1:
+        line += '#'
+    else:
+        line += '.'
+    print('Current CRT row:', line)
+    return line
+
+
+def part_1(data: str) -> int:
+    register = Register('X')
+    values: list[int] = []
+    next_cycle = 20
+    for instruction in data.splitlines():
+        register.cycle += 1
+        next_cycle = add_value(register, values, next_cycle)
+        match instruction.split():
+            case ['noop']:
+                pass
+            case ['addx', n]:  # pylint: disable=invalid-name
+                register.value += int(n)
+                register.cycle += 1
+                next_cycle = add_value(register, values, next_cycle)
+    return sum(values)
+
+
 def part_2(data: str) -> str:
     instructions = list(reversed(data.splitlines()))
-    from itertools import count
-
     sprite_position = 1
     working = False
     line = ''
     lines = []
     print_sprite(sprite_position)
     for cycle in count(1):
-        if cycle == 39:
-            pass
         print()
         pixel_position = cycle - 1
         if cycle > 0:
             pixel_position = pixel_position % 40
+        line = update(cycle, pixel_position, sprite_position, line)
+
         if not working:
             try:
                 instruction = instructions.pop()
             except IndexError:
                 break
-            print('Popped', instruction)
+            print(
+                f'Start cycle {cycle:3}:',
+                f'begin executing {instruction}',
+            )
             match instruction.split():
                 case ['noop']:
-                    print(
-                        f'Start cycle {cycle:3}:',
-                        f'begin executing {instruction}',
-                    )
-                    print(
-                        f'During cycle {cycle:2}:',
-                        f'CRT draws pixel in position {pixel_position}',
-                    )
-                    if abs(pixel_position - sprite_position) <= 1:
-                        line += '#'
-                    else:
-                        line += '.'
-                    print('Current CRT row:', line)
+                    pass
                 case ['addx', n]:  # pylint: disable=invalid-name
-                    print(
-                        f'Start cycle {cycle:3}:',
-                        f'begin executing {instruction}',
-                    )
-                    print(
-                        f'During cycle {cycle:2}:',
-                        f'CRT draws pixel in position {pixel_position}',
-                    )
-                    if abs(pixel_position - sprite_position) <= 1:
-                        line += '#'
-                    else:
-                        line += '.'
-                    print('Current CRT row:', line)
                     working = True
         else:
-            print(
-                f'During cycle {cycle:2}:',
-                f'CRT draws pixel in position {pixel_position}',
-            )
-            if abs(pixel_position - sprite_position) <= 1:
-                line += '#'
-            else:
-                line += '.'
-            print('Current CRT row:', line)
             sprite_position += int(n)
             print(
                 f'End of cycle {cycle:2}:',
@@ -163,12 +145,12 @@ def part_2(data: str) -> str:
             )
             print_sprite(sprite_position)
             working = False
+
         if cycle % 40 == 0:
             lines.append(line)
             line = ''
-        # if cycle == 65: break
     print('\n'.join(lines))
-    return 'EZFPRAKL'  # after looking at output
+    return 'EZFPRAKL'  # [after looking at output]
 
 
 if __name__ == '__main__':
