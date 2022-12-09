fc831d2c69172e1e48e3c3ffe6837a60b3039400
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-12-09 20:32:01 +0000

    Add options to entry point (#17)

--- src/aoc22/__init__.py
+++ src/aoc22/__init__.py
@@ -8,16 +8,13 @@ import coloredlogs
 
 
 LOGGER_FORMAT = '%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s'
-TypeResult = int | str
+TypeResult = int | str | None
 
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     coloredlogs.install(logger=logger, fmt=LOGGER_FORMAT, level=logging.INFO)
     return logger
-
-
-_logger = get_logger(__name__)
 
 
 def read_input(caller_path: str | Path, relative_filepath: str) -> str:
@@ -29,29 +26,37 @@ def read_input(caller_path: str | Path, relative_filepath: str) -> str:
 
 def main(
     file_path: str,
-    solve_part_1: Callable[[str], TypeResult],
-    solve_part_2: Callable[[str], TypeResult],
+    solve_part_1: None | Callable[[str], TypeResult],
+    solve_part_2: None | Callable[[str], TypeResult],
     logger: logging.Logger,
+    examples_only: bool = False,
 ) -> tuple[TypeResult, TypeResult, TypeResult, TypeResult]:
     example = read_input(file_path, 'example.txt')
     data = read_input(file_path, 'input.txt')
 
-    string = f'{Style.BRIGHT}{Fore.YELLOW}PART 1{Style.RESET_ALL}'
-    logger.info(string)
-    example_1 = solve_part_1(example)
-    logger.info('Example: %s', f'{Fore.YELLOW}{example_1}')
-    answer_1 = solve_part_1(data)
-    string = f'{Style.BRIGHT}{Fore.YELLOW}{answer_1}{Style.RESET_ALL}'
-    logger.info('Answer:  %s', string)
+    example_1 = answer_1 = example_2 = answer_2 = None
 
-    logger.info('')
+    if solve_part_1 is not None:
+        string = f'{Style.BRIGHT}{Fore.YELLOW}PART 1{Style.RESET_ALL}'
+        logger.info(string)
+        example_1 = solve_part_1(example)
+        logger.info('Example: %s', f'{Fore.YELLOW}{example_1}')
+        if not examples_only:
+            answer_1 = solve_part_1(data)
+            string = f'{Style.BRIGHT}{Fore.YELLOW}{answer_1}{Style.RESET_ALL}'
+            logger.info('Answer:  %s', string)
 
-    string = f'{Style.BRIGHT}{Fore.CYAN}PART 2{Style.RESET_ALL}'
-    logger.info(string)
-    example_2 = solve_part_2(example)
-    logger.info('Example: %s', f'{Fore.CYAN}{example_2}')
-    answer_2 = solve_part_2(data)
-    string = f'{Style.BRIGHT}{Fore.CYAN}{answer_2}{Style.RESET_ALL}'
-    logger.info('Answer:  %s', string)
+    if solve_part_1 is not None and solve_part_2 is not None:
+        logger.info('')
+
+    if solve_part_2 is not None:
+        string = f'{Style.BRIGHT}{Fore.CYAN}PART 2{Style.RESET_ALL}'
+        logger.info(string)
+        example_2 = solve_part_2(example)
+        logger.info('Example: %s', f'{Fore.CYAN}{example_2}')
+        if not examples_only:
+            answer_2 = solve_part_2(data)
+            string = f'{Style.BRIGHT}{Fore.CYAN}{answer_2}{Style.RESET_ALL}'
+            logger.info('Answer:  %s', string)
 
     return example_1, answer_1, example_2, answer_2
--- src/aoc22/run_day.py
+++ src/aoc22/run_day.py
@@ -13,15 +13,40 @@ app = typer.Typer()
 
 
 @app.command()
-def run_day(day: int) -> None:
-    """Run code for a specific day.
-
-    Args:
-        day: Day between 1 and 25.
-    """
+def run_day(
+    day: int = typer.Argument(  # noqa: B008
+        ...,
+        help='Day between 1 and 25.',
+    ),
+    examples_only: bool = typer.Option(  # noqa: B008
+        False,
+        '-e',
+        help='Run examples only.',
+    ),
+    one_only: bool = typer.Option(  # noqa: B008
+        False,
+        '-1',
+        help='Run part 1 only.',
+    ),
+    two_only: bool = typer.Option(  # noqa: B008
+        False,
+        '-2',
+        help='Run part 2 only.',
+    ),
+) -> None:
+    """Run code for a specific day."""
     day_module_name = f'day_{day:02d}'
     day_module = importlib.import_module(f'aoc22.{day_module_name}')
     day_path = day_module.__file__
     assert isinstance(day_path, str)
 
-    main(day_path, day_module.part_1, day_module.part_2, _logger)
+    solve_part_1 = None if two_only else day_module.part_1
+    solve_part_2 = None if one_only else day_module.part_2
+
+    main(
+        day_path,
+        solve_part_1,
+        solve_part_2,
+        _logger,
+        examples_only=examples_only,
+    )
